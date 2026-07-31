2e4c42cf0492429a1d109013e29f2bf4a9b2cf42
Erik Pellizzon <erikpellizzon1@gmail.com>
2026-07-31 19:33:52 +0200

    Avoid race conditions in http mitm tls config

--- https.go
+++ https.go
@@ -383,6 +383,7 @@ func (proxy *ProxyHttpServer) handleHttps(w http.ResponseWriter, r *http.Request
 						return
 					}
 				}
+				tlsConfig = tlsConfig.Clone()
 
 				if proxy.AllowHTTP2 {
 					if !slices.Contains(tlsConfig.NextProtos, "h2") {
